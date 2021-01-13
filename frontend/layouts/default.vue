295bf13f836f5a9cd5133d720ce3b84e03dbd1a6
nicolas <nicolas_boeuf@hotmail.fr>
2021-01-13 09:09:19 +0100

    bit more

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -477,11 +477,11 @@ export default {
       // console.log('L-default / sendPostMessage / forceFullHeight : ', forceFullHeight)
 
       if (forceFullHeight || this.isMobileWidth) {
-        heightToSend = Math.ceil(this.contentMaxScrollHeight() * 1.05)
+        heightToSend = Math.ceil(this.contentMaxScrollHeight() * 1.06)
       } else {
         heightToSend = this.defaultOdamapHeight
       }
-      console.log("1.05")
+      console.log("1.06")
       // console.log('L-default / sendPostMessage / heightToSend - B : ', heightToSend)
       let messageToIframeParent = {
         // fixedHeight: !forceFullHeight,
