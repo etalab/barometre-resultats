e6439361f827fb09a067f5cf4c9a8115f49d1bd3
Julien_P <jparis.py@gmail.com>
2021-01-13 14:03:28 +0100

    no charts on map views

--- frontend/configs/dev/appConfigRoutes.js
+++ frontend/configs/dev/appConfigRoutes.js
@@ -784,7 +784,7 @@ const COMMON_TEMPLATES = {
                 // },
 
                 // KPI GROUP'S KPI COMPONENTS
-                ...COMMON_KPI_COMPONENTS.components(kpiFamilyOptions, kpiGroupOptions, true), // true => addCharts to map routes
+                ...COMMON_KPI_COMPONENTS.components(kpiFamilyOptions, kpiGroupOptions, false), // true => addCharts to map routes
 
                 {
                   component: 'infoBox',
